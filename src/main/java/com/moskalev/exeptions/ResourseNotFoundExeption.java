7bc0252408f1f6957a35ad7e9197e942fe7da6d6
Vasiliy Velikiy <moskalev36@yandex.ru>
2022-02-12 16:49:42 +0300

    create runtime exeption

--- src/main/java/com/moskalev/exeptions/ResourseNotFoundExeption.java
+++ src/main/java/com/moskalev/exeptions/ResourseNotFoundExeption.java
@@ -1,5 +1,7 @@
 package com.moskalev.exeptions;
-
+/**@version  1.1
+ * @author Vasiliy Moskalev
+ * Class runtimeexeption if some kind of resource not found */
 public class ResourseNotFoundExeption extends  RuntimeException {
 
     public ResourseNotFoundExeption(String message) {
